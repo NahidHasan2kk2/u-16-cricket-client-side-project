cc59fceb1e146710d6e9dbe9cc7f9364bf353759
nahidhasan2kk2 <nahidhasan2kk2@gmail.com>
2026-05-22 20:21:01 +0300

    final deploy before dead line

--- src/app/dashboard/manage-facilities/page.jsx
+++ src/app/dashboard/manage-facilities/page.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 const ManageFacilities = () => {
  return (
-  <div>
-   Manage-facility
+  <div className='text-center bg-gray-200 py-20 m-5 '>
+   <h1 className='text-3xl font-bold opacity-55'>
+    No added manage facility yet !
+   </h1>
   </div>
  );
 };
--- src/components/FacilitiesCard.jsx
+++ src/components/FacilitiesCard.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 import { Card } from '@heroui/react';
 import { DollarSign, Users, MapPin, Clock } from 'lucide-react';
 import Image from 'next/image';
-import Link from 'next/link';
+
 import { useRouter } from 'next/navigation';
 
 const FacilitiesCard = ({ facility }) => {
@@ -22,19 +22,15 @@ const FacilitiesCard = ({ facility }) => {
  } = facility;
 
  const handleViewDetails = () => {
-  const token = typeof window !== "undefined"
-   ? localStorage.getItem("token")
-   : null;
-
-  console.log("TOKEN:", token);
-
-  if (!token) {
-   router.push("/auth/login");
-   return;
-  }
 
   router.push(`/dashboard/all-facilities/${_id}`);
+
  };
+
+ const safeImage =
+  typeof image === "string" && image.startsWith("http")
+   ? image
+   : "/banner5.jpg";
 
  return (
   <Card className="w-full max-w-sm rounded-2xl h-full flex flex-col  shadow-md overflow-hidden hover:shadow-2xl transition">
